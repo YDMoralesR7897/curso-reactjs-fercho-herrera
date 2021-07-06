1b7db3fbc2c59e59f85c48102991f509bb09bae4
Yerson D <Yerson7897@gmail.com>
2021-07-06 07:09:25 -0500

    up

---
+++ intro-javascript/src/Bases/10-fetch-api.js
@@ -0,0 +1,13 @@
+const apiKey = 'YEKopj7lyq5Y4NBsrnvvhzyqgVtFC8v1'
+
+const peticion = fetch(`https://api.giphy.com/v1/gifs/random?apikey=${ apiKey }`);
+
+peticion
+.then(resp => resp.json())
+.then(({data}) =>{
+    const {url} = data.images.original;
+    const img = document.createElement('img');
+    img.src = url;
+    document.body.append(img);
+})
+.catch(console.warn);
